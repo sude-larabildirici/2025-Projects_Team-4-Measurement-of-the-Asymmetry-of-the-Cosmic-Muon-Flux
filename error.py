4c074410004a5441ed9d3896ef82ffeb02064622
CiaranSConnolly <ciaran.connolly@student.uva.nl>
2025-06-18 12:23:45 +0200

    Update error.py
    Meer uitleg gegeven over variabelen

--- error.py
+++ error.py
@@ -1,6 +1,13 @@
 import math
 
-def error_flux(tijd, Solid, Aeff, hoek_graad, N, d, h, b, l, w, le, Err_h, Err_d):
+#d is de hoeveelheid die de scintilator horizontaal beweegt, dus d=h tan(hoek) m
+#h is de hoogte van de tafel, h=0.86 m
+#w is half de breedte van de scintilator, w=0.045 m
+#le is half de lengte van de scintilator, le=0.20 m
+#Aeff is het effectieve oppervlak, en kan constant geschat worden, Aeff=0.56 m^2
+
+
+def error_flux(tijd, Solid, Aeff, hoek_graad, N, d, h, w, le, Err_h, Err_d):
     hoek = hoek_graad*math.pi/180
     cos_hoek = math.cos(hoek)
     sin_hoek = math.sin(hoek)
@@ -15,10 +22,6 @@ def error_flux(tijd, Solid, Aeff, hoek_graad, N, d, h, b, l, w, le, Err_h, Err_d
     par_hoek_d = h/(d^2+h^2)
 
     Err_hoek = math.sqrt((Err_d*par_hoek_d)**2+(Err_h*par_hoek_h)**2)
-    par_Aeff_hoek = (-b*sin_hoek*(l+h))
-
-    Err_Aeff = abs(Err_hoek*par_Aeff_hoek)
-    par_flux_Aeff = -N/(tijd*Solid*(Aeff**2))
 
     #par_Solid_hoek = (4*h*le*w*cos_hoek*((w**2 + h**2)*cos_hoek**2 + le**2)*sin_hoek)/((h**2*cos_hoek**2 + le**2) * (w**2*cos_hoek**2 + h**2*cos_hoek**2 + le**2)**(1.5))
     par_Solid_hoek_num = 2*h*le*w*sec_hoek*((h**2*sec_hoek**2 + 4*w**2*cos_hoek**2 + 4*le**2)*((2*h**2*cos_hoek*sec_hoek**2 + 4*w**2*cos_hoek**3 + 4*le**2*cos_hoek)*tan_hoek + (h**2*sec_hoek**2 + 4*le**2)*sin_hoek))
@@ -32,5 +35,5 @@ def error_flux(tijd, Solid, Aeff, hoek_graad, N, d, h, b, l, w, le, Err_h, Err_d
     Err_Solid = math.sqrt((Err_hoek*par_Solid_hoek)**2+(Err_h*par_Solid_h)**2)
     par_flux_Solid = -(N/(tijd*Solid**2*Aeff))
 
-    Err_flux = math.sqrt((Err_N*par_flux_N)**2+(Err_Aeff*par_flux_Aeff)**2+(Err_Solid*par_flux_Solid)**2)
+    Err_flux = math.sqrt((Err_N*par_flux_N)**2+(Err_Solid*par_flux_Solid)**2)
     return Err_flux
